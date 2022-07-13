f5b46f5e1911cc46350c8804ae5b3b01509918a6
Tikky-Dev <tikkygames@gmail.com>
2022-07-13 20:15:19 +0200

    Fixup: Make BrowserRouter global.

--- 04-front-end/src/index.tsx
+++ 04-front-end/src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import reportWebVitals from './common/reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 reportWebVitals(console.log);
